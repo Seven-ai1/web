9d0b987f296e8e2ddfc9d97db121b7020088b5ef
Seven <74672190+Seven-ai1@users.noreply.github.com>
2023-07-31 19:08:28 +0700

    Add files via upload

---
+++ covido-html/myscript.js
@@ -0,0 +1,91 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+// import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmdPIBoxJN6b8At-98qOgmFYh8u2W_sCQ",
+    authDomain: "authloginsys.firebaseapp.com",
+    databaseURL: "https://authloginsys-default-rtdb.firebaseio.com",
+    projectId: "authloginsys",
+    storageBucket: "authloginsys.appspot.com",
+    messagingSenderId: "888458854858",
+    appId: "1:888458854858:web:c545a4f2d82f301c8f0f33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+let signinButton = document.getElementById("signin-button");
+let signupButton = document.getElementById("signup-button");
+
+
+
+signupButton.addEventListener("click", (e) => {
+  let name = document.getElementById("name").value;
+  let nohp = document.getElementById("nohp").value;
+  let emailSignup = document.getElementById("email_signup").value;
+  let passwordSignup = document.getElementById("psw_signup").value;
+
+  createUserWithEmailAndPassword(auth, emailSignup, passwordSignup)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      set(ref(database, "users/" + user.uid), {
+        name: name,
+        nohp: nohp,
+        email: emailSignup,
+        password: passwordSignup
+      })
+        .then(() => {
+          // Data saved successfully!
+          alert("user telah sukses dibuat");
+        })
+        .catch((error) => {
+          //the write failed
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+
+signinButton.addEventListener("click", (e) => {
+  let emailSignin = document.getElementById("email_signin").value;
+  let passwordSignin = document.getElementById("psw_signin").value;
+  signInWithEmailAndPassword(auth, emailSignin, passwordSignin)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      let lgDate = new Date();
+      update(ref(database, "users/" + user.uid), {
+        last_login: lgDate
+      })
+        .then(() => {
+          // Data saved successfully!
+          //   alert("user telah sukses login");
+          location.href = "mainpage.html";
+        })
+        .catch((error) => {
+          //the write failed
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
